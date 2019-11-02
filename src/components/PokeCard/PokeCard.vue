04bd62340daa59902515eadf9d0b27942136db72
Mariam <mariamkhmahran@hotmail.com>
2019-11-02 02:21:44 +0200

    used global mixin to get pokemon's data

--- src/components/PokeCard/PokeCard.vue
+++ src/components/PokeCard/PokeCard.vue
@@ -1,93 +1,81 @@
 <template>
-  <div class="pokeCard">
-    <div class="cardBody">
-      <div id="Upper">
-        <img id="sprite" :src="spriteURL" />
-      </div>
-      <div class="cardFooter">
-        <span id="pokeNumber">{{pokeNumber}}</span>
-        <br />
-        <span id="pokeName">
-          <b>{{pokeName}}</b>
-        </span>
-        <div id="tags">
-          <Tag :key="type" v-for="type in types" :type="type" />
-        </div>
+  <div class="cardBody" @click="showDetails" :class="{side: this.side}">
+    <div id="Upper">
+      <img id="sprite" :src="spriteURL" />
+    </div>
+    <div class="cardFooter">
+      <span id="pokeNumber">{{pokeNumber}}</span>
+      <br />
+      <span id="pokeName">
+        <b>{{pokeName}}</b>
+      </span>
+      <div id="tags">
+        <Tag :key="type" v-for="type in types" :type="type" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import Tag from "./Tag";
-/* eslint-disable no-console */
 
 export default {
   components: {
     Tag
   },
-  props: ["pokemon"],
+  props: { ID: String, side: Boolean },
   data: () => {
     return {
-      number: "",
+      pokemon: null,
+      pokeName: "",
       spriteURL: "",
       types: []
     };
   },
-  created() {
-    var num = this.pokemon.url.split("/");
-    this.number = num[num.length - 2];
-    this.getPokeSprites();
-    this.getTypes();
+  beforeMount() {
+    this.getPokemon(this.ID).then(p => {
+      this.pokemon = p.pokemon;
+      this.spriteURL = this.pokemon.sprites.front_default;
+      this.types = this.pokemon.types.map(t => t.type.name);
+      this.pokeName = this.pokemon.name;
+    });
   },
   computed: {
     pokeNumber() {
-      var num = this.number;
+      var num = this.ID;
       while (num.length < 3) {
         num = "0" + num;
       }
       return "#" + num;
-    },
-    pokeName() {
-      return this.pokemon.name;
     }
   },
   methods: {
-    getPokeSprites() {
-      axios
-        .get(`https://pokeapi.co/api/v2/pokemon-form/${this.number}/`)
-        .then(r => {
-          this.spriteURL = r.data.sprites.front_default;
-        });
-    },
-    getTypes() {
-      axios.get(this.pokemon.url).then(r => {
-        this.types = r.data.types.map(t => t.type.name);
-      });
+    showDetails() {
+      this.$router.replace({ path: `/pokemon/${this.ID}` });
     }
   }
 };
 </script>
 
 <style scoped>
-.pokeCard {
+.cardBody {
   width: 22%;
   margin: 10px auto;
   transition: all 0.2s ease-in-out;
   cursor: pointer;
-}
-
-.pokeCard:hover {
-  transform: scale(1.03);
-}
-
-.cardBody {
   display: flex;
   flex-direction: column;
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   border-radius: 6px;
+}
+
+.side {
+  width: 45%;
+}
+
+.cardBody:hover {
+  transform: scale(1.03);
 }
 
 .cardFooter {
@@ -124,18 +112,27 @@ export default {
 }
 
 @media only screen and (max-width: 1050px) {
-  .pokeCard {
+  .cardBody {
     width: 32%;
+  }
+  .side {
+    width: 90%;
   }
 }
 @media only screen and (max-width: 800px) {
-  .pokeCard {
+  .cardBody {
     width: 49%;
+  }
+  .side {
+    width: 90%;
   }
 }
 @media only screen and (max-width: 500px) {
-  .pokeCard {
+  .cardBody {
     width: 99%;
+  }
+  .side {
+    width: 90%;
   }
 }
 </style>
